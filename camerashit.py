b2eec6cbc5b9299c7deb1ad645d971e3170707c3
ShengBin-101 <52733750+ShengBin-101@users.noreply.github.com>
2022-06-11 22:31:20 +0800

    Added Canny Edge Detection

--- camerashit.py
+++ camerashit.py
@@ -1,3 +1,4 @@
+from cv2 import Canny
 import numpy as np
 import cv2
 
@@ -12,17 +13,23 @@ while cap.isOpened():
 #img = cv2.imread('faces.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    edge = cv2.Canny(gray,80,100)
+    kernal = np.ones(1)
+    erode = cv2.erode(edge, kernal, iterations=1)
+    dilate = cv2.dilate(erode, kernal, iterations=2)
 
     faces = face_cascade.detectMultiScale(gray, 2, 4)
     for (x,y,w,h) in faces:
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        #roi_gray = gray[y:y+h, x:x+w]
-        #roi_color = img[y:y+h, x:x+w]
-        #eyes = eye_cascade.detectMultiScale(roi_gray)
-        #for (ex,ey,ew,eh) in eyes:
-        #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     cv2.imshow('img',img)
+    cv2.imshow('dilate',dilate)
+    cv2.imshow('gray',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cv2.destroyAllWindows()
